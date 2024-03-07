da02c806a3b720ef5a9de67f3050a8518baef6ae
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-03-07 10:46:14 +0100

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,59 @@
 import Image from "next/image";
-
+import twitter from "../assets/twiiter.svg";
+import telegram from "../assets/telegram.svg";
+import logo from "../assets/logo.svg";
+import pic from "../assets/pic.png";
+import Link from "next/link";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex flex-col items-start w-full h-full lg:min-h-screen p-[10px] lg:p-[20px_100px]">
+      <div className="text-white flex justify-between items-center w-full">
+        <div>
+          <Link href="/">
+            <Image src={logo} alt="logo" />
+          </Link>
+        </div>
+        <div className="flex items-center space-x-[5px] lg:space-x-[10px]">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.telegram.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            <Image src={telegram} alt="twitter" />
+          </a>
+          <a
+            href="https://www.twitter.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src={twitter} alt="twitter" />
           </a>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="border box-shad flex flex-col-reverse lg:flex-row items-start w-full mt-[20px]  rounded-[16px] gap-[20px] lg:h-[500px]">
+        <div className="w-full lg:w-[60%] min-h-full flex flex-col items-start justify-center  px-[15px] lg:px-[30px]">
+          <p className="text-[#FFFF01] font-nick uppercase text-[24px] lg:text-[40px]">
+            Lamps on Blast⚡
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className="w-[90%] text-[20px] font-manrope leading-[32px]">
+            Empowering the trillion dollar blockchain economy with lightning
+            speed multichain trading tools.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          <p className="font-manrope mt-[20px] text-[20px]">V.1 coming soon on <span className="font-[800]">Blast️</span></p>
+          <div className="mt-[40px] w-full grid lg:grid-cols-2 items-center gap-[40px] mb-[50px] lg:mb-0">
+            <div>
+              <p className="text-[14px] text-[#FFFF01] font-nick">Enter {`Password`}</p>
+              <input type="text" className="w-full border border-[#FFFFFF66] bg-transparent h-[56px] rounded-[5px] font-manrope px-[15px] outline-none mt-[5px]" placeholder="Password" />
+            </div>
+            <div>
+              <p className="text-[14px] text-[#FFFF01] font-nick">Enter {`Twitter Username`}</p>
+              <input type="text" className="w-full border border-[#FFFFFF66] bg-transparent h-[56px] rounded-[5px] font-manrope px-[15px] outline-none mt-[5px]" placeholder="@twitter username" />
+            </div>
+          </div>
+        </div>
+        <div className="w-full lg:w-[40%]   rounded-t-[16px] lg:rounded-t-none lg:rounded-r-[16px]">
+          <Image src={pic} className="lg:h-[500px] rounded-t-[16px] lg:rounded-t-none lg:rounded-r-[16px]" alt="pic" />
+        </div>
       </div>
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      fontFamily:{
+        "manrope": ["Manrope", "sans-serif"],
+        "nick" : ["NicoMoji-Regular", "sans-serif"]
+      }
     },
   },
   plugins: [],
