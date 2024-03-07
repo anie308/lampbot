98f40a39e222310a1715117f4ec606e44cc91872
Aniebietabasi <aniebietabasiobo@gmail.com>
2024-03-07 13:34:38 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         </div>
       </div>
       <div className="border box-shad flex flex-col-reverse lg:flex-row items-start w-full mt-[20px]  rounded-[16px] gap-[20px] lg:h-[500px]">
-        <div className="w-full lg:w-[60%] min-h-full flex flex-col items-start justify-center  px-[15px] lg:px-[30px]">
+        <div className="w-full lg:w-[55%] min-h-full flex flex-col items-start justify-center  px-[15px] lg:px-[30px]">
           <p className="text-[#FFFF01] font-nick uppercase text-[24px] lg:text-[40px]">
             Lamps on Blast⚡
           </p>
@@ -40,7 +40,7 @@ export default function Home() {
             speed multichain trading tools.
           </p>
           <p className="font-manrope mt-[20px] text-white text-[20px]">V.1 coming soon on <span className="font-[800]">Blast️</span></p>
-          <div className="mt-[40px] w-full grid lg:grid-cols-2 items-center gap-[40px] mb-[50px] lg:mb-0">
+          <div className="mt-[40px] w-full grid lg:grid-cols-2 items-center gap-[40px] ">
             <div>
               <p className="text-[14px] text-[#FFFF01] font-nick">Enter {`Password`}</p>
               <input type="text" className="w-full border border-[#FFFFFF66] bg-transparent h-[56px] rounded-[5px] font-manrope px-[15px] outline-none mt-[5px]" placeholder="Password" />
@@ -50,9 +50,12 @@ export default function Home() {
               <input type="text" className="w-full border border-[#FFFFFF66] bg-transparent h-[56px] rounded-[5px] font-manrope px-[15px] outline-none mt-[5px]" placeholder="@twitter username" />
             </div>
           </div>
+          <button className="linear w-full h-[56px] mt-[20px] mb-[30px] lg:mb-0 rounded-[8px]">
+            <p className="text-[20px] font-nick text-black">Get early access</p>
+          </button>
         </div>
-        <div className="w-full lg:w-[40%]   rounded-t-[16px] lg:rounded-t-none lg:rounded-r-[16px]">
-          <Image src={pic} className="lg:h-[500px] rounded-t-[16px] lg:rounded-t-none lg:rounded-r-[16px]" alt="pic" />
+        <div className="w-full lg:w-[45%]   rounded-t-[16px] lg:rounded-t-none lg:rounded-r-[16px]">
+          <Image src={pic} className="lg:h-[500px] w-full rounded-t-[16px] lg:rounded-t-none lg:rounded-r-[16px]" alt="pic" />
         </div>
       </div>
     </main>
